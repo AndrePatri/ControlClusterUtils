a39e83558456e0b4e6801050db890469e0f0d40f
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-11-18 19:23:59 +0100

    Signed-off-by: Andrea Patrizi <andreapatrizi1b6e6@gmail.com>

--- control_cluster_bridge/controllers/rhc.py
+++ control_cluster_bridge/controllers/rhc.py
@@ -71,7 +71,7 @@ class RHController(ABC):
 
         if self._debug:
             np.seterr(over='ignore')
-            
+
         self._n_nodes = n_nodes
         self._dt = dt
         self._n_intervals = self._n_nodes - 1 
@@ -793,7 +793,7 @@ class RHController(ABC):
                 self._contact_force_base_loc_aux[:, :]=np.nan_to_num(self._contact_force_base_loc_aux/self._contact_f_scale,
                                                             nan=1e3,
                                                             posinf=1e3, neginf=-1e3) # ensure finiteness
-                np.clip(self._contact_force_base_loc_aux, out=self._contact_force_base_loc_aux, a_min=-1e3, a_max=1e3) # in place clipping
+                self._contact_force_base_loc_aux[:, :]=np.clip(self._contact_force_base_loc_aux, a_min=-1e3, a_max=1e3) # in place clipping
                 self.robot_cmds.contact_wrenches.set(data=np.clip(self._contact_force_base_loc_aux/self._contact_f_scale, a_min=-1e3, a_max=1e3), 
                     data_type="f", 
                     robot_idxs=self.controller_index_np,
